8e776d350529cd8f801a8f52205a27c1282df7aa
yuzuru imai <yuzuruimai0121@gmail.com>
2023-12-13 20:45:51 +0900

    fast

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(function(){
+
+	$(".overlay").hide();
+	$(".work_detail").hide();
+
+	//smooth_scroll
+	$('a[href^="#"]:not([href="#"])').click(function(){
+
+        var target = $($(this).attr("href")).offset().top;
+
+        $("html,body").animate({scrollTop: target},500);
+
+        return false;
+    });
+
+	//scroll_anime
+	$(window).scroll(function () {
+		const windowHeight = $(window).height();
+		const scroll = $(window).scrollTop();
+		
+		$('.element').each(function () {
+			const targetPosition = $(this).offset().top;
+			if (scroll > targetPosition - windowHeight + 100) {
+			$(this).addClass("is-fadein");
+			}
+		});
+	});
+
+	$(".work_item").click(function(){
+		$(this).children(".overlay").fadeIn().children(".work_detail").fadeIn();
+
+		$(".overlay").click(function(event){
+			if (!$(event.target).closest(".work_detail").length){
+				$(".work_detail").fadeOut();
+				$(this).fadeOut();
+			}
+			return false;
+		});
+		return false;
+	});
+	
+});
+
+	
+
