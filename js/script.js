354064bfee5799aaf3b028f3b4a190c9a75f20b2
yuzuru imai <yuzuruimai0121@gmail.com>
2024-08-21 23:04:34 +0900

    リンクを押した時にリンク先に飛ばなかったコードを修正。 ホバー時に画像を少し縮小するように追加。

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
-$(function(){
+$(function () {
 
 	$(".overlay").hide();
 	$(".work_detail").hide();
 
 	//smooth_scroll
-	$('a[href^="#"]:not([href="#"])').click(function(){
+	$('a[href^="#"]:not([href="#"])').click(function () {
 
-        var target = $($(this).attr("href")).offset().top;
+		var target = $($(this).attr("href")).offset().top;
 
-        $("html,body").animate({scrollTop: target},500);
+		$("html,body").animate({ scrollTop: target }, 500);
 
-        return false;
-    });
+		return false;
+	});
 
 	//scroll_anime
 	$(window).scroll(function () {
 		const windowHeight = $(window).height();
 		const scroll = $(window).scrollTop();
-		
+
 		$('.element').each(function () {
 			const targetPosition = $(this).offset().top;
 			if (scroll > targetPosition - windowHeight + 100) {
-			$(this).addClass("is-fadein");
+				$(this).addClass("is-fadein");
 			}
 		});
 	});
 
-	$(".work_item").click(function(){
-		$(this).children(".overlay").fadeIn().children(".work_detail").fadeIn();
+	$(".work_item_top").click(function () {
+		$(this).next(".overlay").fadeIn().next(".work_detail").fadeIn();
 
-		$(".overlay").click(function(event){
-			if (!$(event.target).closest(".work_detail").length){
+		$(".overlay").click(function (event) {
+			if (!$(event.target).closest(".work_detail").length) {
 				$(".work_detail").fadeOut();
 				$(this).fadeOut();
 			}
@@ -38,8 +38,8 @@ $(function(){
 		});
 		return false;
 	});
-	
+
 });
 
-	
+
 
